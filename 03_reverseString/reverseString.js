78346c8f159b9107f998d07818b76da6e37ce8f8
David Spiegel <84484532+davidspiegs@users.noreply.github.com>
2024-06-28 17:39:22 -0400

    Completed assignment 03, reverseString and all tests passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const strRev =  string.split('').reverse().join('');
+    return strRev;
 };
 
 // Do not edit below this line
