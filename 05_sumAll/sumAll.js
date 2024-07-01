e7a760a3f07db4fb1695cde0c2b4a863dee1c547
David Spiegel <84484532+davidspiegs@users.noreply.github.com>
2024-07-01 17:14:35 -0400

    For assignment 05 SumAll, I have created a function that seems to work for all use cases. Passed test 1 so far. Will check other tests.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = (num1, num2) => {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    } else {
+        let smallerValue = Math.min(num1, num2);
+        let largerValue = Math.max(num1, num2);
+        let sum = 0;
 
+        for (let i = smallerValue; i <= largerValue; i++) {
+            sum += i;
+    }
+    return sum;
+    }
 };
 
+console.log(sumAll(3,5));
 // Do not edit below this line
 module.exports = sumAll;
