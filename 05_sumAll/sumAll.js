5923e260f14ad23ca14e5fa39dbdb2e787ee6a16
David Spiegel <84484532+davidspiegs@users.noreply.github.com>
2024-07-01 17:21:13 -0400

    Added statement that returns ERROR if any input value is a negative number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,13 @@
 const sumAll = (num1, num2) => {
+    // Need to make sure both values are numbers
     if(typeof num1 !== 'number' || typeof num2 !== 'number') {
         return "ERROR";
+
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+
     } else {
+        
         let smallerValue = Math.min(num1, num2);
         let largerValue = Math.max(num1, num2);
         let sum = 0;
@@ -12,7 +18,5 @@ const sumAll = (num1, num2) => {
     return sum;
     }
 };
-
-console.log(sumAll(3,5));
 // Do not edit below this line
 module.exports = sumAll;
