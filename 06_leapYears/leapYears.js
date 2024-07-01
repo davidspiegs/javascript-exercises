4f49a7ef9ab4c214e291770d14e1bab14292a61e
David Spiegel <84484532+davidspiegs@users.noreply.github.com>
2024-07-01 18:12:13 -0400

    Built a V1, haven't tested yet. We'll see if it works.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        // If divisible by 100, not a leap year, unless also divisible by 400.
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
